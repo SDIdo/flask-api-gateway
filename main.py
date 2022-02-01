bdcd992e380b53e802ef3df6cb7f65bdfdf74799
SDido <bookidodo@gmail.com>
2022-02-01 15:34:02 +0200

    added api

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from pymongo import MongoClient
+from passbox import password
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Hello World"}
+class Todos(Resource):
 
-api.add_resource(HelloWorld, "/home")
+    def connect_to_mongo(self, database):
+        client = MongoClient(f"mongodb+srv://SDido:{password}@cluster0.xtvv1.mongodb.net/{database}?retryWrites=true&w=majority")
+        db = client.get_database(database)
+        records = db.todos
+        return records
+
+    def get(self):
+        records = self.connect_to_mongo('Family')
+        all_records = list(records.find())
+        return {"data": all_records}
+
+    # def post(self):
+
+api.add_resource(Todos, "/family-todos")
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -4,5 +4,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "home")
+response = requests.get(BASE + "family-todos")
 print(response.json())
